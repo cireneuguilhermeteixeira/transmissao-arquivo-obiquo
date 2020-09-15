c3e7f0fbfcba2e2f0d4f8d92b8ffdcac526cddea
cireneuguilhermepontotel <cireneu@pontotel.com.br>
2020-09-15 19:22:26 -0300

    ajustes main

--- client/src/main/java/sample/Main.java
+++ client/src/main/java/sample/Main.java
@@ -10,17 +10,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         System.out.println("Hello Client");
-
-//        spaceHandlerInstance.writeEnviroment("ambiente 1");
-//        spaceHandlerInstance.writeEnviroment("ambiente 2");
-//        Device device = new Device();
-//        device.setAddress("aaaaaa");
-//        device.setLocalization(new Localization("123","456"));
-//        device.setName("dispositivo 1");
-//        spaceHandlerInstance.writeConnectionSolicitation(device);
-//        device.setLocalization(new Localization("456","789"));
-//        spaceHandlerInstance.writeChangePosition(device);
-
         Socket clientSocket = new Socket("localhost", 5000);
         MessageHandlerSocket messageHandlerSocket = new MessageHandlerSocket(clientSocket);
         messageHandlerSocket.getStatus();
